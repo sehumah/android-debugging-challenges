59bccd1f25408425e566e1fa9f317ceee5b9b114
sehumah <sehumah@protonmail.com>
2022-03-08 01:42:20 -0600

    fixed change background activity bug

--- app/src/main/java/com/codepath/debuggingchallenges/activities/ChangeBackgroundActivity.kt
+++ app/src/main/java/com/codepath/debuggingchallenges/activities/ChangeBackgroundActivity.kt
@@ -1,19 +1,35 @@
 package com.codepath.debuggingchallenges.activities
 
+import android.annotation.SuppressLint
 import android.graphics.Color
 import android.os.Bundle
+import android.util.Log
 import android.view.View
+import android.widget.Button
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.codepath.debuggingchallenges.R
 
+
+private const val TAG = "ChangeBackgroundActivity"
 class ChangeBackgroundActivity : AppCompatActivity() {
     private var oldColor = Color.BLUE
+    @SuppressLint("LongLogTag")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_change_background)
+        // get reference to layout view
+        val view = findViewById<View>(R.id.content)
+        // get reference to button and set click listener to button
+        val btnGo = findViewById<Button>(R.id.btnGo)
+        btnGo.setOnClickListener {
+            onGoClick(view)
+            // Toast.makeText(this, "GO button clicked!", Toast.LENGTH_LONG).show()
+        }
+        Log.i(TAG, "in $TAG")
     }
 
-    fun onGoClick(view: View?) {
+    fun onGoClick(view: View?) { // originally had "view: View?" as parameter
         val mainView = findViewById<View>(android.R.id.content)
         mainView.setBackgroundColor(nextColor)
     }
@@ -25,3 +41,4 @@ class ChangeBackgroundActivity : AppCompatActivity() {
             return newColor
         }
 }
+
