ad4b3b9e990adb54d659da49e86f814b73d570bb
sehumah <sehumah@protonmail.com>
2022-03-08 00:37:00 -0600

    fixed current day activity bug

--- app/src/main/java/com/codepath/debuggingchallenges/activities/CurrentDayActivity.kt
+++ app/src/main/java/com/codepath/debuggingchallenges/activities/CurrentDayActivity.kt
@@ -3,22 +3,34 @@ package com.codepath.debuggingchallenges.activities
 import androidx.appcompat.app.AppCompatActivity
 import android.widget.TextView
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import com.codepath.debuggingchallenges.R
+import org.w3c.dom.Text
 import java.util.*
 
+
+private const val TAG = "CurrentDayActivity"
 class CurrentDayActivity : AppCompatActivity() {
     var tvDay: TextView? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_current_day)
         tvDay = findViewById<View>(R.id.tvDay) as TextView
-        tvDay?.setText(dayOfMonth)
+//        tvDay?.setText(dayOfMonth)  // original line
+        tvDay?.text = dayOfMonth.toString()
+        Log.i(TAG, "info : in CurrentDayActivity.kt")
+        Log.e(TAG, "error: in CurrentDayActivity.kt")
     }
 
-    private val dayOfMonth: Int
+    private val dayOfMonth: String
         get() {
             val cal = Calendar.getInstance()
-            return cal[Calendar.DAY_OF_MONTH]
+//            return cal[Calendar.DAY_OF_MONTH]
+            /* below is my solution */
+            val month = cal[Calendar.MONTH]
+            val day = cal[Calendar.DAY_OF_MONTH]
+            val year = cal[Calendar.YEAR]
+            return "$month/$day/$year"
         }
 }
