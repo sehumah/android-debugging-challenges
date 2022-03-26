9ee6675902434e3bfe83a3d4c5706e6b28efb7ba
sehumah <sehumah@protonmail.com>
2022-03-26 18:39:00 -0500

    use a mutable list for the movies array and modify file to get movies

--- app/src/main/java/com/codepath/debuggingchallenges/activities/MoviesActivity.kt
+++ app/src/main/java/com/codepath/debuggingchallenges/activities/MoviesActivity.kt
@@ -5,6 +5,7 @@ import androidx.recyclerview.widget.RecyclerView
 import com.codepath.debuggingchallenges.adapters.MoviesAdapter
 import android.os.Bundle
 import android.util.Log
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.codepath.debuggingchallenges.R
 import com.codepath.asynchttpclient.AsyncHttpClient
 import com.codepath.asynchttpclient.callback.JsonHttpResponseHandler
@@ -13,10 +14,12 @@ import com.codepath.debuggingchallenges.models.Movie
 import okhttp3.Headers
 import java.util.ArrayList
 
+
+private const val TAG = "MoviesActivity"
 class MoviesActivity : AppCompatActivity() {
     private var rvMovies: RecyclerView? = null
     var adapter: MoviesAdapter? = null
-    var movies: List<Movie>? = null
+    var movies = mutableListOf<Movie>() // List<Movie>? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -24,21 +27,24 @@ class MoviesActivity : AppCompatActivity() {
         rvMovies = findViewById(R.id.rvMovies)
 
         // Create the adapter to convert the array to views
-        val adapter = MoviesAdapter(movies)
+        adapter = MoviesAdapter(movies)
 
         // Attach the adapter to a ListView
         rvMovies?.adapter = adapter
+        rvMovies?.layoutManager = LinearLayoutManager(this)
         fetchMovies()
+        Log.i(TAG, "in MoviesActivity's onCreate")
     }
 
     private fun fetchMovies() {
-        val url = " https://api.themoviedb.org/3/movie/now_playing?api_key="
+        val url = " https://api.themoviedb.org/3/movie/now_playing?api_key=$API_KEY"
         val client = AsyncHttpClient()
         client[url, null, object : JsonHttpResponseHandler() {
             override fun onSuccess(statusCode: Int, headers: Headers, response: JSON) {
                 try {
                     val moviesJson = response.jsonObject.getJSONArray("results")
-                    movies = Movie.fromJSONArray(moviesJson)
+                    movies.addAll(Movie.fromJSONArray(moviesJson))
+                    adapter?.notifyDataSetChanged()
                 } catch (e: JSONException) {
                     e.printStackTrace()
                 }
@@ -51,6 +57,6 @@ class MoviesActivity : AppCompatActivity() {
     }
 
     companion object {
-        private const val API_KEY = "a07e22bc18f5cb106bfe4cc1f83ad8ed"
+        private const val API_KEY = R.string.API_KEY.toString()
     }
 }
